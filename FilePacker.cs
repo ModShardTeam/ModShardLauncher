c7aa124abd8fcc339f2da0f5f242484982a637a5
remyCases <remy.cases@gmail.com>
2024-02-11 19:45:30 +0100

    [Minor] add comment about the fileversion fix

--- FilePacker.cs
+++ FilePacker.cs
@@ -31,6 +31,8 @@ namespace ModShardLauncher
             Write(fs, "MSLM");
             Log.Information("Writting header...");
             
+            // work around to find the FileVersion of ModShardLauncher.dll for single file publishing
+            // see: https://github.com/dotnet/runtime/issues/13051
             Assembly assembly = Assembly.GetExecutingAssembly() ?? throw new NullReferenceException("ExecutingAssembly");
             string mod_version = "v" + FileVersionInfo.GetVersionInfo(assembly.Location).FileVersion;
             Write(fs, mod_version);
