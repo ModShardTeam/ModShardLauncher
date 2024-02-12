ab5cfc5f81b3199cedbd2ae295f76b5f38a3d943
remyCases <remy.cases@gmail.com>
2024-02-12 20:59:04 +0100

    [Major] LocalizationItem, tool to easily inject localization strings to the table_consumables

--- ModUtils/TableUtils.cs
+++ ModUtils/TableUtils.cs
@@ -73,4 +73,66 @@ namespace ModShardLauncher
             } 
         }
     }
+    public class LocalizationItem
+    {
+        public string OName { get; set; }
+        public Dictionary<ModLanguage, string> ConsumableName { get; set; } = new();
+        public Dictionary<ModLanguage, string> ConsumableID { get; set; } = new();
+        public Dictionary<ModLanguage, string> ConsumableDescription { get; set; } = new();
+        public LocalizationItem(string oName)
+        {
+            OName = oName;
+        }
+        public LocalizationItem(string oName, Dictionary<ModLanguage, string> dictName, Dictionary<ModLanguage, string> dictID, Dictionary<ModLanguage, string> dictDescription)
+        {
+            OName = oName;
+            Localization.SetDictionary(ConsumableName, dictName);
+            Localization.SetDictionary(ConsumableID, dictID);
+            Localization.SetDictionary(ConsumableDescription, dictDescription);
+        }
+        public LocalizationItem(string oName, string valuesName, string valuesID, string valuesDescription)
+        {
+            OName = oName;
+            Localization.SetDictionary(ConsumableName, valuesName);
+            Localization.SetDictionary(ConsumableID, valuesID);
+            Localization.SetDictionary(ConsumableDescription, valuesDescription);
+        }
+        static private string CreateLine(string oName, Dictionary<ModLanguage, string> dict)
+        {
+            string line = oName;
+            foreach (KeyValuePair<ModLanguage, string> kp in dict)
+            {
+                line += ";";
+                line += kp.Value;
+            }
+            return line + ";//;";
+        }
+        private IEnumerable<string> EditTable(IEnumerable<string> table)
+        {
+            foreach (string line in table)
+            {
+                if (line.Contains("consum_name_end"))
+                {
+                    yield return CreateLine(OName, ConsumableName);
+                }
+                else if (line.Contains("consum_mid_end"))
+                {
+                    yield return CreateLine(OName, ConsumableID);
+                }
+                else if (line.Contains("consum_desc_end"))
+                {
+                    yield return CreateLine(OName, ConsumableDescription);
+                }
+                yield return line;
+            }
+        }
+        private IEnumerable<string> EditTable(string tableName)
+        {
+            return EditTable(Msl.ThrowIfNull(ModLoader.GetTable(tableName)));
+        }
+        public void InjectTable()
+        {
+            ModLoader.SetTable(EditTable("gml_GlobalScript_table_consumables").ToList(), "gml_GlobalScript_table_consumables");
+        }
+    }
 }
