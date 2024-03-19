55ec154ff38de3931e3e0f68d0a3d989f3fa690e
remyCases <remy.cases@gmail.com>
2024-03-19 13:31:14 +0100

    [Major] rework json parsing for loot table

--- ModUtils/LootUtils.cs
+++ ModUtils/LootUtils.cs
@@ -46,53 +46,66 @@ namespace ModShardLauncher
                 file = file_text_open_read(""loot_table.json""); 
                 json = file_text_read_string(file);
                 var data = json_parse(json);
-                var lootStruct = ds_map_find_value(data, argument0);
 
-                if (__is_undefined(lootStruct))
+                if (!variable_struct_exists(data, argument0))
                 {
-                    scr_actionsLogUpdate(""nothing"");
+                    scr_actionsLogUpdate(""cant find: "" + string(argument0));
                     return 0;
                 }
+                var lootStruct = variable_struct_get(data, argument0);
 
-                var guaranteedItems = ds_map_find_value(lootStruct, ""GuaranteedItems"");
+                var objectName = """";
+                var obj = -1;
 
-                if (__is_undefined(guaranteedItems))
+                if (!variable_struct_exists(lootStruct, ""GuaranteedItems""))
                 {
                     scr_actionsLogUpdate(""no guaranteedItems"");
                     return 0;
                 }
+                var guaranteedItems = variable_struct_get(lootStruct, ""GuaranteedItems"");
+                var sizeGuaranteedItems = array_length(guaranteedItems);
 
-                var sizeGuaranteedItems = ds_list_size(guaranteedItems);
                 for (var i = 0; i < sizeGuaranteedItems; i += 1)
                 {
-                    scr_inventory_add_item(ds_list_find_value(guaranteedItems, i));
+                    objectName = guaranteedItems[i];
+                    obj = asset_get_index(objectName)
+                    if (obj > -1)
+                    {
+                        scr_inventory_add_item(obj);
+                    }
+                    else
+                    {
+                        scr_actionsLogUpdate(""invalid object "" + string(objectName));
+                    }
                 }
-
-                var randomLootMin = ds_map_find_value(lootStruct, ""RandomLootMin"");
-                var randomLootMax = ds_map_find_value(lootStruct, ""RandomLootMax"");
-                var emptyWeight = ds_map_find_value(lootStruct, ""EmptyWeight"");
-
-                if (__is_undefined(randomLootMin) || __is_undefined(randomLootMax) || __is_undefined(emptyWeight))
+    
+                if (!variable_struct_exists(lootStruct, ""RandomLootMin"") || !variable_struct_exists(lootStruct, ""RandomLootMax"") || !variable_struct_exists(lootStruct, ""EmptyWeight""))
                 {
                     scr_actionsLogUpdate(""no int"");
                     return 0;
                 }
-                
-                var iteration = randomLootMin + irandom(randomLootMax - randomLootMin);
-                var tableLootWeight = ds_map_find_value(lootStruct, ""TableLootWeight"");
-                var tableLootItems = ds_map_find_value(lootStruct, ""TableLootItems"");
 
-                if (__is_undefined(tableLootWeight) || __is_undefined(tableLootItems))
+                var randomLootMin = variable_struct_get(lootStruct, ""RandomLootMin"");
+                var randomLootMax = variable_struct_get(lootStruct, ""RandomLootMax"");
+                var emptyWeight = variable_struct_get(lootStruct, ""EmptyWeight"");
+
+                var iteration = randomLootMin + irandom(randomLootMax - randomLootMin);
+                scr_actionsLogUpdate(""iteration "" + string(iteration));
+
+                if (!variable_struct_exists(lootStruct, ""TableLootWeight"") || !variable_struct_exists(lootStruct, ""TableLootItems""))
                 {
                     scr_actionsLogUpdate(""no tableLoot"");
                     return 0;
                 }
 
-                var sizeTableLoot = ds_map_size(tableLootWeight);
+                var tableLootWeight = variable_struct_get(lootStruct, ""TableLootWeight"");
+                var tableLootItems = variable_struct_get(lootStruct, ""TableLootItems"");
+
+                var sizeTableLoot = array_length(tableLootWeight);
                 var totalWeight = emptyWeight;
                 for (var i = 0; i < sizeTableLoot; i += 1)
                 {
-                    totalWeight += ds_list_find_value(tableLootWeight, i);
+                    totalWeight += tableLootWeight[i];
                 }
 
                 scr_actionsLogUpdate(""totalWeight "" + string(totalWeight));
@@ -106,7 +119,7 @@ namespace ModShardLauncher
 
                     for (var i = 0; i < sizeTableLoot; i += 1)
                     {
-                        cumulativeWeight += ds_list_find_value(tableLootWeight, i)
+                        cumulativeWeight += tableLootWeight[i]
                         if (randomWeight < cumulativeWeight) 
                         {
                             index = i;
@@ -117,26 +130,34 @@ namespace ModShardLauncher
                     if (index != -1)
                     {
                         scr_actionsLogUpdate(""found "" + string(index));
-                        scr_inventory_add_item(ds_list_find_value(tableLootItems, index));
+                        objectName = tableLootItems[index];
+                        obj = asset_get_index(objectName)
+                        if (obj > -1)
+                        {
+                            scr_inventory_add_item(obj);
+                        }
+                        else
+                        {
+                            scr_actionsLogUpdate(""invalid object "" + string(objectName));
+                        }
                     }
                     else 
                     {
                         scr_actionsLogUpdate(""found empty"");
                     }
                 }
-                
             }";
 
             AddFunction(lootFunction, "scr_resolve_loot_table");
 
             LoadGML("gml_Object_o_chest_p_Alarm_1")
                 .MatchFrom("script_execute")
-                .InsertBelow("scr_resolve_loot_table(other.name)")
+                .InsertBelow("scr_resolve_loot_table(object_get_name(other.object_index))")
                 .Save();
                 
             LoadGML("gml_Object_c_container_Other_10")
                 .MatchFrom("script_execute")
-                .InsertBelow("scr_resolve_loot_table(other.name)")
+                .InsertBelow("scr_resolve_loot_table(object_get_name(other.object_index))")
                 .Save();
         }
     }
