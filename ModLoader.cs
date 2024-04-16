bc973ea16432a2005d2bc07f0f11a437c98ed768
Nylux <nylux42@gmail.com>
2024-04-16 17:49:48 +0200

    Disabled Mantodea's Weapon injection as it overrides table injections

--- ModLoader.cs
+++ ModLoader.cs
@@ -216,8 +216,8 @@ namespace ModShardLauncher
         {
             PatchInnerFile();
             PatchMods();
-            SetTable(Weapons, "gml_GlobalScript_table_weapons");
-            SetTable(WeaponDescriptions, "gml_GlobalScript_table_weapons_text");
+            // SetTable(Weapons, "gml_GlobalScript_table_weapons");
+            // SetTable(WeaponDescriptions, "gml_GlobalScript_table_weapons_text");
         }
         internal static void PatchInnerFile()
         {
