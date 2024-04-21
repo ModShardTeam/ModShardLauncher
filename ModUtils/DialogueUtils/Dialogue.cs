19f805c60b8bf4159086fda749068445dc7c58dd
Nylux <nylux42@gmail.com>
2024-04-21 07:49:43 +0200

    Split classes and utils in different files, created builder class for chained method calls

---
+++ ModUtils/DialogueUtils/Dialogue.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+
+namespace ModShardLauncher.ModUtils;
+
+public class DE2Dialogue
+{
+    public string Name { get; set; }
+    public List<DE2Topic> DialogueTopics { get; set; }
+        
+    public DE2Dialogue(string name)
+    {
+        this.Name = name;
+        this.DialogueTopics = new List<DE2Topic>();
+    }
+        
+    public DE2Dialogue AddTopic(params DE2Topic[] topics)
+    {
+        foreach (DE2Topic topic in topics)
+            this.DialogueTopics.Add(topic);
+        return this;
+    }
+}
+    
+public class DE2Topic
+{
+    public string Name { get; set; }
+    public List<DE2Option> TopicOptions { get; set; }
+        
+    public DE2Topic(string name, params DE2Option[] topicOptions)
+    {
+        this.Name = name;
+        this.TopicOptions = new List<DE2Option>(topicOptions);
+    }
+        
+    public DE2Topic AddOption(params DE2Option[] options)
+    {
+        foreach (DE2Option option in options)
+            this.TopicOptions.Add(option);
+        return this;
+    }
+}
+
+public class DE2Option
+{
+    public string? Text { get; set; }
+    public string? Script { get; set; }
+    public string? NextTopic { get; set; }
+
+        
+    public DE2Option(string? text, string? script, string? nextTopic)
+    {
+        this.Text = text;
+        this.Script = script;
+        this.NextTopic = nextTopic;
+    }
+}
---
+++ ModUtils/DialogueUtils/DialogueUtils.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ModShardLauncher.ModUtils
+{
+    /* the builder class `AddDE2Dialogue` should lead to the following syntax :
+     
+     new Msl.AddDE2Dialogue("DialogueName")
+    .AddTopic("topic0")
+    .AddOptionToLastTopic("sayHi", "scr_myScript", "topic1")
+    .AddOptionToLastTopic("Leave", null, null)
+    .AddTopic("topic1")
+    .AddOptionToLastTopic("back", null, "topic0")
+    .Build();
+    
+    */
+    public static partial class Msl
+    {
+        public class AddDE2Dialogue
+        {
+            private DE2Dialogue _dialogue;
+            
+            public AddDE2Dialogue(string name)
+            {
+                _dialogue = new DE2Dialogue(name);
+            }
+
+            public AddDE2Dialogue AddTopic(string name)
+            {
+                DE2Topic topic = new(name);
+                _dialogue.DialogueTopics.Add(topic);
+                return this;
+            }
+
+            public AddDE2Dialogue AddOptionToLastTopic(string text, string script, string nextTopic)
+            {
+                DE2Option option = new(text, script, nextTopic);
+                DE2Topic? lastTopic = _dialogue.DialogueTopics.LastOrDefault();
+                lastTopic?.AddOption(option);
+                return this;
+            }
+            
+            public void Build()
+            {
+                // Creating a HashSet to avoid looping through topics twice
+                HashSet<string> topicNames = new(_dialogue.DialogueTopics.Select(t => t.Name));
+                
+                // Making sure all NextTopic reference valid topics
+                foreach (DE2Topic topic in _dialogue.DialogueTopics)
+                {
+                    foreach (DE2Option option in topic.TopicOptions)
+                    {
+                        if (option.NextTopic != null && !topicNames.Contains(option.NextTopic))
+                            throw new ArgumentException($"Option {option.Text} in topic {topic.Name} references a non-existent topic {option.NextTopic} in dialogue {_dialogue.Name}");
+                    }
+                }
+                
+                // Actually build the .de2 file
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
