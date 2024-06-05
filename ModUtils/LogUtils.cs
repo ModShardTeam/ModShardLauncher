2e1cb20fbf18e123c1fb7e8148e79bd8a9fbda24
remyCases <remy.cases@gmail.com>
2024-06-05 23:09:01 +0200

    [Major] fixing log file names missing zeros, fixing log file being only 16ko, fixing wrapping of log

--- ModUtils/LogUtils.cs
+++ ModUtils/LogUtils.cs
@@ -18,32 +18,50 @@ if (global._msl_log != noone)
 {
     if (global._msl_log.buf != noone)
     {
-        buffer_write(global._msl_log.buf, buffer_text, ""["" + time + ""]: "" + argument0 + ""\n"");
+        var string_log = ""["" + time + ""]: "" + argument0 + ""\n"";
+        var len_log = string_byte_length(string_log);
+
+        if (len_log > global._msl_log.size)
+        {
+            string_log = string_copy(string_log, 1, global._msl_log.size - 1) + ""\n"";
+            len_log = string_byte_length(string_log);
+        }
+
+        if (len_log + global._msl_log.cur_size > global._msl_log.size)
+        {
+            scr_msl_log_save();
+            global._msl_log.nfile += 1;
+            global._msl_log.cur_size = 0;
+        }
+
+        buffer_write(global._msl_log.buf, buffer_text, string_log);
+        global._msl_log.cur_size += len_log;
+        
+        if (global._msl_log.timer == noone || !instance_exists(global._msl_log.timer))
+        {
+            var t = instance_create(0, 0, o_msl_timer);
+            t.end_time = 5;
+            t.func = gml_Script_scr_msl_log_save;
+
+            global._msl_log.timer = t.id;
+        }
     }
     else
     {
-        scr_actionsLogUpdate(""msl log buff does not exist. Please report that bug with the MSL devs."");
+        scr_actionsLogUpdate(""msl log buff does not exist. Please report that bug to the MSL devs."");
     }
 }
 else
 {
-    scr_actionsLogUpdate(""msl log does not exist. Please report that bug with the MSL devs."");
+    scr_actionsLogUpdate(""msl log does not exist. Please report that bug to the MSL devs."");
 }
-
-if (global._msl_log.timer == noone || !instance_exists(global._msl_log.timer))
-{
-    var t = instance_create(0, 0, o_msl_timer);
-    t.end_time = 10;
-    t.func = gml_Script_scr_msl_log_save;
-
-    global._msl_log.timer = t.id;
-}
-
 }";
 
         string mslLogSave = @"function scr_msl_log_save()
 {
-    buffer_save_async(global._msl_log.buf, global._msl_log.name, 0, 16384);
+    var nfile_name = global._msl_log.name + ""_"" + string(global._msl_log.nfile) + "".txt"";
+    buffer_save_async(global._msl_log.buf, nfile_name, 0, global._msl_log.cur_size);
+    instance_destroy(global._msl_log.timer);
 }";
 
         UndertaleGameObject timer = Msl.AddObject("o_msl_timer", isPersistent: true);
@@ -67,11 +85,14 @@ if (cumulative_time > end_time)
 
         UndertaleGameObject log = Msl.AddObject("o_msl_log", isPersistent: true);
         Msl.AddNewEvent(log, @"
-buf = buffer_create(16384, buffer_wrap, 1);
+size = 1000000
+buf = buffer_create(size, buffer_wrap, 1);
+cur_size = 0
+nfile = 0
 
 var curr_time = date_current_datetime();
-var format_time = string(date_get_year(curr_time)) + string(date_get_month(curr_time)) + string(date_get_day(curr_time)) + ""_"" + string(date_get_hour(curr_time)) + string(date_get_minute(curr_time));
-name = ""Logs/msl_log_"" + format_time + "".txt"";
+var format_time = string_format(date_get_year(curr_time), 2, 0) + string_format(date_get_month(curr_time), 2, 0) + string_format(date_get_day(curr_time), 2, 0) + ""_"" + string_format(date_get_hour(curr_time), 2, 0) + string_format(date_get_minute(curr_time), 2, 0);
+name = ""Logs/msl_log_"" + string_replace_all(format_time, "" "", ""0"");
 
 timer = -4
 ", 
@@ -81,13 +102,7 @@ timer = -4
         Msl.AddFunction(mslLog, "scr_msl_log");
         Msl.LoadGML(Msl.EventName("o_gameLoader", EventType.Create, 0))
             .MatchAll()
-            .InsertBelow(@"
-if (!directory_exists(""Logs""))
-{
-    directory_create(""Logs"");
-}
-global._msl_log = instance_create(0, 0, o_msl_log);
-")
+            .InsertBelow(@"global._msl_log = instance_create(0, 0, o_msl_log);")
             .Save();       
     }
 }
