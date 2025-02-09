cbab24ad0d68a35f0569fe6113962e26c24f7677
Nylux <nylux42@gmail.com>
2025-02-09 21:25:26 +0100

    [Minor] Added Hints + Prologue localizable tables

--- ModUtils/TableUtils/Localizable/LocalizableHints.cs
+++ ModUtils/TableUtils/Localizable/LocalizableHints.cs
@@ -1,6 +1,33 @@
-﻿namespace ModShardLauncher;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Serilog;
 
-public class LocalizableHints
+namespace ModShardLauncher;
+
+public partial class Msl
 {
-    
+    public static void InjectTableLocalizableHints(string id, LocalizedStrings text)
+    {
+        // Table filename
+        const string tableName = "gml_GlobalScript_table_hints";
+        
+        // Load table if it exists
+        List<string> table = ThrowIfNull(ModLoader.GetTable(tableName));
+        
+        // Prepare line
+        string newline = $"{id};{text.Russian};{text.English};{text.Chinese};{text.German};{text.SpanishLatam};{text.French};{text.Italian};{text.Portuguese};{text.Polish};{text.Turkish};{text.Japanese};{text.Korean};";
+        
+        // Find hook in table
+        (int ind, string? foundLine) = table.Enumerate().FirstOrDefault(x => x.Item2.Contains("text_end"));
+        
+        // Add line to table
+        if (foundLine != null)
+        {
+            table.Insert(ind, newline);
+            ModLoader.SetTable(table, tableName);
+            Log.Information($"Injected Hint {id} into table {tableName}");
+        }
+        else
+            Log.Error($"Failed to inject Hint {id} into table {tableName}");
+    }
 }
---
+++ ModUtils/TableUtils/Localizable/LocalizablePrologue.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Serilog;
+
+namespace ModShardLauncher;
+
+public partial class Msl
+{
+    public static void InjectTableLocalizablePrologue(int id, LocalizedStrings text, string? voiceOver = null)
+    {
+        // Table filename
+        const string tableName = "gml_GlobalScript_table_prologue";
+        
+        // Load table if it exists
+        List<string> table = ThrowIfNull(ModLoader.GetTable(tableName));
+        
+        // Prepare line
+        string newline = $"{id};{text.Russian};{text.English};{text.Chinese};{text.German};{text.SpanishLatam};{text.French};{text.Italian};{text.Portuguese};{text.Polish};{text.Turkish};{text.Japanese};{text.Korean};\\;{voiceOver};";
+        
+        // Find hook in table
+        (int ind, string? foundLine) = table.Enumerate().FirstOrDefault(x => x.Item2.Contains("prologue_text_end"));
+        
+        // Add line to table
+        if (foundLine != null)
+        {
+            table.Insert(ind, newline);
+            ModLoader.SetTable(table, tableName);
+            Log.Information($"Injected Prologue Text {id} into table {tableName}");
+        }
+        else
+            Log.Error($"Failed to inject Prologue Text {id} into table {tableName}");
+    }
+}
